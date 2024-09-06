599e5a2ca4d71cb6a96e0a83565bb4dc64cb7ac7
Kmore1515 <kylebradleymorris@usf.edu>
2024-09-05 20:45:27 -0400

    Test Datasets

--- bill_splitter.js
+++ bill_splitter.js
@@ -1,21 +1,32 @@
-const billamount = 50; // This number is changed by user based on the bill amount.
-const tip = billamount >50 ? billamount * 0.20 : billamount * 0.15; // This calculated the tip based on if the bill is more or less than 50.
+let billamount = 50; // This number is changed by user based on the bill amount.
+let tip = billamount >50 ? billamount * 0.20 : billamount * 0.15; // This calculated the tip based on if the bill is more or less than 50.
 console.log(`The calculated tip is $${tip.toFixed(2)}`); // Output of tip amount.
 
-const totalamount = billamount + tip;
+let totalamount = billamount + tip;
 console.log(`Bill Amount = $${billamount.toFixed(2)}`); // This uses previously stated billamount from 1st task, Displays bill amount.
 console.log(`Tip = $${tip.toFixed(2)}`); // This also uses previously stated tip from 1st task, Displays tip amount.
 console.log(`Total Amount = $${totalamount.toFixed(2)}`); // Displays total cost amount.
 
 function calculatetip(billamount) {
-     const tip = billamount >50 ? billamount * 0.20 : billamount * 0.15; // The same tip calculator as used earlier.
+     let tip = billamount >50 ? billamount * 0.20 : billamount * 0.15; // The same tip calculator as used earlier.
      return tip };
-const totalbill = 100; // This is our input slot for the total bill.
-const tipamount = calculatetip(totalbill);
+let totalbill = 100; // This is our input slot for the total bill.
+let tipamount = calculatetip(totalbill);
 console.log(`A bill with the value of $${totalbill.toFixed(2)}, the tip amount would be $${tipamount.toFixed(2)}`); //Output slot for total bill with calculated tip.
 
-const bills = [45, 50, 65, 75, 100]; // Array of the total bills
-const tips = bills.map(bill => calculatetip(bill)); // Array of tip amounts
-const totals = bills.map((bill, index) => bill + tips[index]); // Array of total amounts.
-console.log("Bills", bills, "Tips", tips, "Totals", totals ); // A console log output with them all together.
+let bills = [45, 50, 65, 75, 100]; // Array of the total bills
+let tips = bills.map(bill => calculatetip(bill)); // Array of tip amounts
+let totals = bills.map((bill, index) => bill + tips[index]); // Array of total amounts.
+console.log("Bills:", bills, "Tips:", tips, "Totals:", totals); // A console log output with them all together.
 
+function calculatebilldetails(bills) {
+    let tips = bills.map(bill => calculatetip(bill))
+    let totals = bills.map((bill, index) => bill + tips[index]);
+    return { tips, totals}}
+bills = [275, 40, 430]; // Inputs of Dataset1
+let Dataset1 = calculatebilldetails(bills);
+console.log("DataSet 1:", "Bills:", bills, "Tips:", Dataset1.tips, "Totals:", Dataset1.totals); // Outputs of Dataset1
+
+bills = [125, 555, 44]; // Inputs of Dataset2
+let Dataset2 = calculatebilldetails(bills);
+console.log("DataSet 2:", "Bills:", bills, "Tips:", Dataset2.tips, "Totals:", Dataset2.totals); //Outputs of Dataset2
